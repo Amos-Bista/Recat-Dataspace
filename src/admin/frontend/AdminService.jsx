3d3f6692427763651917614124279d242210b6f3
binyul <jiibinyul@gmail.com>
2024-05-03 11:40:29 +0530

    completed admin service

--- src/admin/frontend/AdminService.jsx
+++ src/admin/frontend/AdminService.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import AdminNav from "../../component/adminHome/adminNav";
-import { Button } from "@mui/material";
+import Serviceherosec from "../../component/adminHome/serviceherosec";
+import Serviceaccordin from "../../component/adminHome/serviceaccordin";
+import Servicefeatureplans from "../../component/adminHome/servicefeatureplans";
 
 const AdminService = () => {
   return (
     <main className="flex gap-10 align-middle  bg-[#0D5077]  py-28">
-      <AdminNav />
+      <div className="ml-1">
+        <AdminNav />
+      </div>
       <div className="w-full px-2 py-6 mr-10 bg-gray-300 rounded-lg">
-        <div className="flex justify-between align-middle">
-          <h1>Service</h1>
-          <Button variant="contained" className="flex items-end align-end">
-            Add New +
-          </Button>
-        </div>
-
+          <Serviceherosec />
+          <Serviceaccordin />
+          <Servicefeatureplans />
         
       </div>
     </main>
