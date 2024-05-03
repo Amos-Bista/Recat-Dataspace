424adbc746c88272e3d419821c3265e903524e32
Swornim Shrestha <swornimshrestha639@gmail.com>
2024-05-03 16:04:40 +0545

    aboutCardDesign

--- src/component/about/aboutcard.jsx
+++ src/component/about/aboutcard.jsx
@@ -15,17 +15,17 @@ const Aboutcard = () => {
   }, []);
 
   return (
-    <div className="px-8 py-8 flex flex-row flex-wrap gap-[70px]   ">
+    <div className="px-10 py-8 flex flex-row flex-wrap gap-x-[160px]  gap-y-[90px]  ">
       {Array.isArray(rowData) &&
         rowData.map((row, index) => (
           <Card
             key={index}
             sx={{
-              width: 680,
-              height: 440,
+              width: 605,
+              height: 441,
               paddingX: 4,
               borderRadius: 3,
-              boxShadow: "2px 4px 6px rgba(0, 0, 0, 0.1)",
+              boxShadow: "12px 12px 16px rgba(0, 0, 0, 0.1)",
               backgroundColor: "#fafbfa",
               marginLeft: index === rowData.length - 1 ? "26%" : 0, // Set marginLeft to 30% for the last item
             }}
@@ -36,6 +36,8 @@ const Aboutcard = () => {
                 paddingTop: "10.25%",
                 backgroundSize: "contain",
                 marginTop: 0,
+                Width: 100.88,
+                Height: 91.67,
               }}
               image={row[0].image}
               title={row[0].title}
@@ -50,6 +52,9 @@ const Aboutcard = () => {
                   display: "flex",
                   justifyContent: "center",
                   paddingY: 0,
+                  fontWeight: 500,
+                  fontSize: "24px",
+                  lineHeight: "29px",
                 }}
               >
                 {row[0].title}
@@ -63,6 +68,10 @@ const Aboutcard = () => {
                   justifyContent: "center",
                   minWidth: 30,
                   paddingX: 4,
+
+                  fontSize: "16px",
+                  lineHeight: "29px",
+                  fontWeight: 300,
                 }}
               >
                 {row[0].description}
