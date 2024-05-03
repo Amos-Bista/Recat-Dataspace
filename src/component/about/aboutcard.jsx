5c414b6f4880c9059eb4c4a0709feb39c780155c
Swornim Shrestha <swornimshrestha639@gmail.com>
2024-05-03 16:21:09 +0545

    for anynumber of element

--- src/component/about/aboutcard.jsx
+++ src/component/about/aboutcard.jsx
@@ -8,77 +8,68 @@ const Aboutcard = () => {
   useEffect(() => {
     // Fetch data from JSON file
     const data = Object.values(aboutData);
-    console.log(data);
 
     // Set rowData state with all data from JSON
     setRowData(data);
   }, []);
 
   return (
-    <div className="px-10 py-8 flex flex-row flex-wrap gap-x-[160px]  gap-y-[90px]  ">
-      {Array.isArray(rowData) &&
-        rowData.map((row, index) => (
-          <Card
-            key={index}
-            sx={{
-              width: 605,
-              height: 441,
-              paddingX: 4,
-              borderRadius: 3,
-              boxShadow: "12px 12px 16px rgba(0, 0, 0, 0.1)",
-              backgroundColor: "#fafbfa",
-              marginLeft: index === rowData.length - 1 ? "26%" : 0, // Set marginLeft to 30% for the last item
-            }}
-          >
-            <CardMedia
+    <div className="px-10 py-8 flex flex-wrap justify-center gap-x-[160px] gap-y-[90px]">
+      <div className="flex flex-wrap justify-center gap-x-[160px] gap-y-[90px]">
+        {Array.isArray(rowData) &&
+          rowData.map((row, index) => (
+            <Card
+              key={index}
               sx={{
-                height: 0,
-                paddingTop: "10.25%",
-                backgroundSize: "contain",
-                marginTop: 0,
-                Width: 100.88,
-                Height: 91.67,
+                width: 605,
+                height: 441,
+                paddingX: 4,
+                borderRadius: 3,
+                boxShadow: "12px 12px 16px rgba(0, 0, 0, 0.1)",
+                backgroundColor: "#fafbfa",
               }}
-              image={row[0].image}
-              title={row[0].title}
-            />
-            <CardContent sx={{ paddingTop: 2 }}>
-              <Typography
-                gutterBottom
-                variant="h4"
-                component="div"
+            >
+              <CardMedia
                 sx={{
-                  textAlign: "justify",
-                  display: "flex",
-                  justifyContent: "center",
-                  paddingY: 0,
-                  fontWeight: 500,
-                  fontSize: "24px",
-                  lineHeight: "29px",
+                  height: 0,
+                  paddingTop: "10.25%",
+                  backgroundSize: "contain",
                 }}
-              >
-                {row[0].title}
-              </Typography>
-              <Typography
-                variant="h6"
-                color="black"
-                sx={{
-                  textAlign: "justify",
-                  display: "flex",
-                  justifyContent: "center",
-                  minWidth: 30,
-                  paddingX: 4,
-
-                  fontSize: "16px",
-                  lineHeight: "29px",
-                  fontWeight: 300,
-                }}
-              >
-                {row[0].description}
-              </Typography>
-            </CardContent>
-          </Card>
-        ))}
+                image={row[0].image}
+                title={row[0].title}
+              />
+              <CardContent sx={{ paddingTop: 2 }}>
+                <Typography
+                  gutterBottom
+                  variant="h4"
+                  component="div"
+                  sx={{
+                    textAlign: "center",
+                    fontWeight: 500,
+                    fontSize: "24px",
+                    lineHeight: "29px",
+                  }}
+                >
+                  {row[0].title}
+                </Typography>
+                <Typography
+                  variant="h6"
+                  color="black"
+                  sx={{
+                    textAlign: "justify",
+                    minWidth: 30,
+                    paddingX: 4,
+                    fontSize: "16px",
+                    lineHeight: "29px",
+                    fontWeight: 300,
+                  }}
+                >
+                  {row[0].description}
+                </Typography>
+              </CardContent>
+            </Card>
+          ))}
+      </div>
     </div>
   );
 };
