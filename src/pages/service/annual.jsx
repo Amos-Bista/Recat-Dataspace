925b1a14e9156d9462d08c7c16ff4c1b41932976
khadkabj07 <khadkabj07@gmail.com>
2024-05-06 17:09:51 +0545

    fix according

--- src/pages/service/annual.jsx
+++ src/pages/service/annual.jsx
@@ -10,14 +10,14 @@ const  Annual = () => {
       <AnnualHerosection />
       <div className="w-100% flex justify-center ">
         <div className="px-[4%] my-[5%]">
-          <h1 className="text-5xl font-bold ">
-            Why Choose Dataspace Co-location Services?
+          <h1 className="text-4xl font-bold ml-0">
+            Why Choose Annual Maintenance Service?
           </h1>
-          <ul className="pt-16 pl-6 text-3xl font-medium leading-relaxed list-decimal">
+          <ul className="pt-12 pl-6 text-3xl font-medium leading-relaxed list-decimal">
             <ColocationAccordian />
           </ul>
         </div>
-        <img src="/Annual.png" alt="" />
+        <img src="/Annual.png" alt="" style={{ maxWidth: "500px", maxHeight: "500px" }} />
       </div>
       <Plans />
     </main>
--- src/pages/service/enterprise.jsx
+++ src/pages/service/enterprise.jsx
@@ -8,14 +8,14 @@ const Enterprise = () => {
       < EnterpriseHerosection/>
       <div className="w-100% flex justify-center ">
         <div className="px-[4%] my-[5%]">
-          <h1 className="text-5xl font-bold ">
-            Why Choose Dataspace Co-location Services?
+          <h1 className="text-4xl font-bold ">
+            Why Choose Enterprises Secure Email Service?
           </h1>
           <ul className="pt-16 pl-6 text-3xl font-medium leading-relaxed list-decimal">
             <ColocationAccordian />
           </ul>
         </div>
-        <img src="/enterprise.png" alt="" />
+        <img src="/enterprise.png" alt="" style={{ maxWidth: "500px", maxHeight: "500px" }} />
       </div>
       <Plans />
     </main>
