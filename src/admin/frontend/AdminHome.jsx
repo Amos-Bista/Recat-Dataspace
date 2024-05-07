3f15f77d4502c835e522d25cc728c8d4764d98ad
binyul <jiibinyul@gmail.com>
2024-05-07 14:31:41 +0530

    popup

--- src/admin/frontend/AdminHome.jsx
+++ src/admin/frontend/AdminHome.jsx
@@ -8,8 +8,6 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import AdminNav from "../../component/adminHome/adminNav";
-import Herosec from "../../component/adminHome/herosec";
-import ContactInfo from "../../component/adminHome/contactInfo";
 import HomeHero from "../../component/adminHome/homehero";
 
 function createData(name, calories, fat, carbs, protein) {
@@ -33,7 +31,7 @@ const AdminHome = () => {
           </Button>
           
         </div>
-        {/* <TableContainer component={Paper}>
+        <TableContainer component={Paper}>
           <Table sx={{ minWidth: 650 }} aria-label="simple table">
             <TableHead>
               <TableRow>
@@ -61,7 +59,7 @@ const AdminHome = () => {
               ))}
             </TableBody>
           </Table>
-        </TableContainer> */}
+        </TableContainer>
         <div className="w-full px-2 py-6 mr-10 bg-gray-300 rounded-lg">
           <HomeHero />
         
