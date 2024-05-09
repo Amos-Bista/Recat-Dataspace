f284eb97bbe01eee89cbf97a670576b6d0f489ef
Swornim Shrestha <swornimshrestha639@gmail.com>
2024-05-09 11:49:02 +0545

    adminContactDataPosttoDB

--- src/component/adminContact/contactadmintable.jsx
+++ src/component/adminContact/contactadmintable.jsx
@@ -1,3 +1,229 @@
+// // import React, { useState, useEffect } from "react";
+// // import Table from "@mui/material/Table";
+// // import TableBody from "@mui/material/TableBody";
+// // import TableCell from "@mui/material/TableCell";
+// // import TableContainer from "@mui/material/TableContainer";
+// // import TableHead from "@mui/material/TableHead";
+// // import TableRow from "@mui/material/TableRow";
+// // import Paper from "@mui/material/Paper";
+// // import Button from "@mui/material/Button";
+// // import Delete from "../../component/adminHome/Delete";
+// // import Edit from "../../component/adminHome/Edit";
+// // import ContactAdd from "./contactadd";
+
+// // const ContactAdminTable = () => {
+// //   const [rows, setRows] = useState([]);
+
+// //   const addData = (data) => {
+// //     setRows([...rows, data]);
+// //   };
+
+// //   const handleDelete = (index) => {
+// //     const updatedRows = [...rows];
+// //     updatedRows.splice(index, 1);
+// //     setRows(updatedRows);
+// //   };
+// //   return (
+// //     <main>
+// //       <div className="flex justify-between align-middle">
+// //         <h3 className="my-8 text-2xl font-[400] text-[#0D5077]  text-[34px]  ">
+// //           Contact Information
+// //         </h3>
+// //         <Button>
+
+// //           <ContactAdd addData={addData} />
+// //         </Button>
+// //       </div>
+// //       <div className="">
+// //         <TableContainer component={Paper}>
+// //           <Table sx={{ minWidth: 650 }} aria-label="simple table">
+// //             <TableHead>
+// //               <TableRow>
+// //                 <TableCell align="center">Number</TableCell>
+// //                 <TableCell align="center">Mail</TableCell>
+// //                 <TableCell align="center">Address</TableCell>
+// //                 <TableCell align="center">Edit</TableCell>
+// //                 <TableCell align="center">Delete</TableCell>
+// //               </TableRow>
+// //             </TableHead>
+// //             <TableBody>
+// //               {rows.map((row, index) => (
+// //                 <TableRow key={index}>
+// //                   <TableCell align="center">{row.Number}</TableCell>
+// //                   <TableCell align="center">{row.Mail}</TableCell>
+// //                   <TableCell align="center">{row.Address}</TableCell>
+// //                   <TableCell align="center">
+// //                     <Button sx={{ margin: 2 }}>
+// //                       <Edit />
+// //                     </Button>
+// //                   </TableCell>
+// //                   <TableCell align="center">
+// //                     <Button sx={{ margin: 2 }}>
+// //                       <Delete onDelete={() => handleDelete(index)} />
+// //                     </Button>
+// //                   </TableCell>
+// //                 </TableRow>
+// //               ))}
+// //             </TableBody>
+// //           </Table>
+// //         </TableContainer>
+// //       </div>
+// //     </main>
+// //   );
+// // };
+
+// // export default ContactAdminTable;
+// import React, { useState, useEffect } from "react";
+// import Table from "@mui/material/Table";
+// import TableBody from "@mui/material/TableBody";
+// import TableCell from "@mui/material/TableCell";
+// import TableContainer from "@mui/material/TableContainer";
+// import TableHead from "@mui/material/TableHead";
+// import TableRow from "@mui/material/TableRow";
+// import Paper from "@mui/material/Paper";
+// import Button from "@mui/material/Button";
+// import Delete from "../../component/adminHome/Delete";
+// import Edit from "../../component/adminHome/Edit";
+// import ContactAdd from "./contactadd";
+
+// const ContactAdminTable = () => {
+//   // const [rows, setRows] = useState([]);
+
+//   // const addData = (data) => {
+//   //   setRows([...rows, data]);
+//   // };
+
+//   // const handleDelete = (index) => {
+//   //   const updatedRows = [...rows];
+//   //   updatedRows.splice(index, 1);
+//   //   setRows(updatedRows);
+//   // };
+//   return (
+//     <main>
+//       <div className="flex justify-between align-middle">
+//         <h3 className="my-8 text-2xl font-[400] text-[#0D5077]  text-[34px]  ">
+//           Contact Information
+//         </h3>
+//         <Button>
+//           <ContactAdd />
+//           {/* <ContactAdd addData={addData} /> */}
+//         </Button>
+//       </div>
+//       {/* <div className="">
+//         <TableContainer component={Paper}>
+//           <Table sx={{ minWidth: 650 }} aria-label="simple table">
+//             <TableHead>
+//               <TableRow>
+//                 <TableCell align="center">Number</TableCell>
+//                 <TableCell align="center">Mail</TableCell>
+//                 <TableCell align="center">Address</TableCell>
+//                 <TableCell align="center">Edit</TableCell>
+//                 <TableCell align="center">Delete</TableCell>
+//               </TableRow>
+//             </TableHead>
+//             <TableBody>
+//               {rows.map((row, index) => (
+//                 <TableRow key={index}>
+//                   <TableCell align="center">{row.Number}</TableCell>
+//                   <TableCell align="center">{row.Mail}</TableCell>
+//                   <TableCell align="center">{row.Address}</TableCell>
+//                   <TableCell align="center">
+//                     <Button sx={{ margin: 2 }}>
+//                       <Edit />
+//                     </Button>
+//                   </TableCell>
+//                   <TableCell align="center">
+//                     <Button sx={{ margin: 2 }}>
+//                       <Delete onDelete={() => handleDelete(index)} />
+//                     </Button>
+//                   </TableCell>
+//                 </TableRow>
+//               ))}
+//             </TableBody>
+//           </Table>
+//         </TableContainer>
+//       </div> */}
+//     </main>
+//   );
+// };
+
+// export default ContactAdminTable;
+// import React, { useState, useEffect } from "react";
+// import Table from "@mui/material/Table";
+// import TableBody from "@mui/material/TableBody";
+// import TableCell from "@mui/material/TableCell";
+// import TableContainer from "@mui/material/TableContainer";
+// import TableHead from "@mui/material/TableHead";
+// import TableRow from "@mui/material/TableRow";
+// import Paper from "@mui/material/Paper";
+// import Button from "@mui/material/Button";
+// import Delete from "../../component/adminHome/Delete";
+// import Edit from "../../component/adminHome/Edit";
+// import ContactAdd from "./contactadd";
+
+// const ContactAdminTable = () => {
+//   const [rows, setRows] = useState([]);
+
+//   const addData = (data) => {
+//     setRows([...rows, data]);
+//   };
+
+//   const handleDelete = (index) => {
+//     const updatedRows = [...rows];
+//     updatedRows.splice(index, 1);
+//     setRows(updatedRows);
+//   };
+//   return (
+//     <main>
+//       <div className="flex justify-between align-middle">
+//         <h3 className="my-8 text-2xl font-[400] text-[#0D5077]  text-[34px]  ">
+//           Contact Information
+//         </h3>
+//         <Button>
+
+//           <ContactAdd addData={addData} />
+//         </Button>
+//       </div>
+//       <div className="">
+//         <TableContainer component={Paper}>
+//           <Table sx={{ minWidth: 650 }} aria-label="simple table">
+//             <TableHead>
+//               <TableRow>
+//                 <TableCell align="center">Number</TableCell>
+//                 <TableCell align="center">Mail</TableCell>
+//                 <TableCell align="center">Address</TableCell>
+//                 <TableCell align="center">Edit</TableCell>
+//                 <TableCell align="center">Delete</TableCell>
+//               </TableRow>
+//             </TableHead>
+//             <TableBody>
+//               {rows.map((row, index) => (
+//                 <TableRow key={index}>
+//                   <TableCell align="center">{row.Number}</TableCell>
+//                   <TableCell align="center">{row.Mail}</TableCell>
+//                   <TableCell align="center">{row.Address}</TableCell>
+//                   <TableCell align="center">
+//                     <Button sx={{ margin: 2 }}>
+//                       <Edit />
+//                     </Button>
+//                   </TableCell>
+//                   <TableCell align="center">
+//                     <Button sx={{ margin: 2 }}>
+//                       <Delete onDelete={() => handleDelete(index)} />
+//                     </Button>
+//                   </TableCell>
+//                 </TableRow>
+//               ))}
+//             </TableBody>
+//           </Table>
+//         </TableContainer>
+//       </div>
+//     </main>
+//   );
+// };
+
+// export default ContactAdminTable;
+"use client";
 import React, { useState, useEffect } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -14,15 +240,35 @@ import ContactAdd from "./contactadd";
 const ContactAdminTable = () => {
   const [rows, setRows] = useState([]);
 
-  const addData = (data) => {
-    setRows([...rows, data]);
-  };
+  // const addData = (data) => {
+  //   setRows([...rows, data]);
+  // };
 
   const handleDelete = (index) => {
     const updatedRows = [...rows];
     updatedRows.splice(index, 1);
     setRows(updatedRows);
   };
+
+  useEffect(() => {
+    fetchData();
+  }, []); // Empty dependency array to ensure this effect runs only once
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "http://172.16.100.109:8282/contacts/allContacts"
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      const data = await response.json();
+      setRows(data); // Update the state with fetched data
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   return (
     <main>
       <div className="flex justify-between align-middle">
@@ -30,7 +276,8 @@ const ContactAdminTable = () => {
           Contact Information
         </h3>
         <Button>
-          <ContactAdd addData={addData} />
+          <ContactAdd />
+          {/* <ContactAdd addData={addData} /> */}
         </Button>
       </div>
       <div className="">
@@ -48,9 +295,9 @@ const ContactAdminTable = () => {
             <TableBody>
               {rows.map((row, index) => (
                 <TableRow key={index}>
-                  <TableCell align="center">{row.Number}</TableCell>
-                  <TableCell align="center">{row.Mail}</TableCell>
-                  <TableCell align="center">{row.Address}</TableCell>
+                  <TableCell align="center">{row.phoneNum}</TableCell>
+                  <TableCell align="center">{row.email}</TableCell>
+                  <TableCell align="center">{row.address}</TableCell>
                   <TableCell align="center">
                     <Button sx={{ margin: 2 }}>
                       <Edit />
