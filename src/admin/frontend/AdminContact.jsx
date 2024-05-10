af67c71d4dc78a23d0bf08d7880d27bf8143110b
Amos-Bista <120589997+Amos-Bista@users.noreply.github.com>
2024-05-10 15:33:14 +0545

    color

--- src/admin/frontend/AdminContact.jsx
+++ src/admin/frontend/AdminContact.jsx
@@ -8,7 +8,7 @@ const AdminContact = () => {
     <>
       <main className="flex align-middle  bg-[#0D5077]  py-28">
           <AdminNav />
-        <div className="w-full px-2 py-6 mr-10 bg-gray-300 rounded-lg">
+        <div className="w-full px-2 py-6 mr-10 bg-gray-200 rounded-lg">
           <HomeHero />
           <ContactAdminTable />
         </div>
--- src/admin/frontend/AdminService.jsx
+++ src/admin/frontend/AdminService.jsx
@@ -9,7 +9,7 @@ const AdminService = () => {
   return (
     <main className="flex  align-middle  bg-[#0D5077]  py-28">
       <AdminNav />
-      <div className="w-full px-2 py-6 bg-gray-300 rounded-lg mr-14">
+      <div className="w-full px-2 py-6 bg-gray-200 rounded-lg mr-14">
         <Serviceherosec />
         <Serviceaccordin />
         <Servicefeatureplans />
