e289244df4fd0ba5bccd93db040c07704f2d4094
Swornim Shrestha <swornimshrestha639@gmail.com>
2024-05-13 12:36:37 +0545

    adminAbout

--- src/admin/frontend/AdminAbout.jsx
+++ src/admin/frontend/AdminAbout.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import Herosec from "../../component/adminHome/herosec";
 import AdminNav from "../../component/adminHome/adminNav";
 import AboutCardTable from "../../component/adminAbout/aboutCardtable";
+import HomeHero from "../../component/adminHome/homehero";
 
 const AdminAbout = () => {
   return (
     <main className="  flex align-middle bg-[#0D5077] py-28">
       <AdminNav />
       <div className="w-full px-2 py-6 bg-gray-200 rounded-lg mr-14">
-        <Herosec />
+        <HomeHero />
         <AboutCardTable />
       </div>
     </main>
