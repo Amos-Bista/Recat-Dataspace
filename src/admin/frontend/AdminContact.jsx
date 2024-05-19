070745cac38cda752490fc8e11bdea4d881620c0
khadkabj07 <khadkabj07@gmail.com>
2024-05-19 14:40:00 +0545

    layout

--- src/admin/frontend/AdminContact.jsx
+++ src/admin/frontend/AdminContact.jsx
@@ -8,7 +8,7 @@ const AdminContact = () => {
     <>
       <main className="flex align-middle  bg-gray-200  py-28">
         <AdminNav />
-        <div className="w-full px-2 py-6 mr-10 bg-gray-200 rounded-lg">
+        <div className="w-full px-2 py-6 mr-10 bg-gray-200 rounded-lg mt-[-40px]">
           <HomeHero />
           <ContactAdminTable />
         </div>
