773dd7cfe966d2740ea75926b8f2514420b12b37
Amos-Bista <120589997+Amos-Bista@users.noreply.github.com>
2024-06-12 14:57:15 +0545

    POST GET DELETE compeleted

--- src/component/adminContact/contactinfoadd.jsx
+++ src/component/adminContact/contactinfoadd.jsx
@@ -1,165 +1,3 @@
-// import {
-//   Button,
-//   Dialog,
-//   DialogActions,
-//   DialogContent,
-//   DialogTitle,
-//   TextField,
-//   Grid,
-//   Typography,
-// } from "@mui/material";
-// import React, { useState } from "react";
-// import IconButton from "@mui/material/IconButton";
-// import CloseIcon from "@mui/icons-material/Close";
-
-// const ContactInfoAdd = ({ handleAddContact }) => {
-//   const [open, setOpen] = useState(false);
-//   const [phoneNum, setPhoneNum] = useState("");
-//   const [email, setEmail] = useState("");
-//   const [address, setAddress] = useState("");
-//   const [response, setResponse] = useState("");
-//   const functionOnPopUp = () => {
-//     setOpen(true);
-//   };
-//   const closePopUp = () => {
-//     setOpen(false);
-//   };
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const response = await fetch(
-//         `${process.env.REACT_APP_API_BASE_URL}/contacts/addContacts`,
-//         {
-//           method: "POST",
-//           headers: {
-//             "Content-Type": "application/json",
-//           },
-//           body: JSON.stringify({ phoneNum, email, address }),
-//         }
-//       );
-//       if (response.ok) {
-//         setResponse("Contact registeresd");
-//         alert("Form submitted successfully!");
-//         handleAddContact();
-//       } else {
-//         throw new Error("Network response was not ok");
-//       }
-//     } catch (error) {
-//       console.error("Error:", error);
-//       setResponse("Error posting data.");
-//       alert("Error submitting form. Please try again.");
-//     }
-//     setOpen(false);
-//   };
-
-//   return (
-//     <>
-//       <Button onClick={functionOnPopUp} color="primary" variant="contained">
-//         Add New +
-//       </Button>
-//       <Dialog open={open} onClose={closePopUp} fullWidth maxWidth="md">
-//         <DialogTitle
-//           style={{ color: "#0c5177", textAlign: "center", fontSize: "30px" }}
-//         >
-//           Contact Information
-//           <IconButton
-//             aria-label="close"
-//             onClick={closePopUp}
-//             sx={{
-//               position: "absolute",
-//               right: 8,
-//               top: 8,
-//               color: (theme) => theme.palette.grey[500],
-//             }}
-//           >
-//             <CloseIcon />
-//           </IconButton>
-//         </DialogTitle>
-//         <DialogContent>
-//           <Grid container spacing={4} padding={5}>
-//             <Grid item xs={6}>
-//               <Typography variant="h6" gutterBottom>
-//                 Phone Number:
-//               </Typography>
-//             </Grid>
-//             <Grid item xs={6}>
-//               <TextField
-//                 label="Enter number"
-//                 variant="outlined"
-//                 fullWidth
-//                 value={phoneNum}
-//                 onChange={(e) => setPhoneNum(e.target.value)}
-//               />
-//             </Grid>
-//             <Grid item xs={6}>
-//               <Typography variant="h6" gutterBottom>
-//                 Email:
-//               </Typography>
-//             </Grid>
-//             <Grid item xs={6}>
-//               <TextField
-//                 label="Enter mail"
-//                 variant="outlined"
-//                 fullWidth
-//                 value={email}
-//                 onChange={(e) => setEmail(e.target.value)}
-//               />
-//             </Grid>
-//             <Grid item xs={6}>
-//               <Typography variant="h6" gutterBottom>
-//                 Address
-//               </Typography>
-//             </Grid>
-//             <Grid item xs={6}>
-//               <TextField
-//                 label="Enter address"
-//                 variant="outlined"
-//                 fullWidth
-//                 value={address}
-//                 onChange={(e) => setAddress(e.target.value)}
-//               />
-//             </Grid>
-//           </Grid>
-//         </DialogContent>
-//         <DialogActions
-//           style={{
-//             display: "flex",
-//             gap: "460px",
-//             MaxwWidth: "895px",
-//           }}
-//         >
-//           <Button
-//             variant="contained"
-//             onClick={closePopUp}
-//             style={{
-//               backgroundColor: "#FF0000",
-//               marginLeft: "auto",
-//               marginRight: "auto",
-//             }}
-//           >
-//             UNPUBLISH
-//           </Button>
-//           <Button
-//             onClick={handleSubmit}
-//             style={{
-//               backgroundColor: "#0c5177",
-//               color: "#fff",
-//               marginLeft: "auto",
-//               marginRight: "auto",
-//             }}
-//             variant="contained"
-//           >
-//             PUBLISH
-//           </Button>
-//         </DialogActions>
-//       </Dialog>
-//     </>
-//   );
-// };
-
-// export default ContactInfoAdd;
-
 import {
   Button,
   Dialog,
@@ -173,8 +11,6 @@ import {
 import React, { useState, useRef } from "react";
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
-import axios from "axios";
-import { json } from "react-router-dom";
 
 const ContactInfoAdd = ({ handleAddContact }) => {
   const [open, setOpen] = useState(false);
@@ -184,12 +20,23 @@ const ContactInfoAdd = ({ handleAddContact }) => {
   const [address, setAddress] = useState("");
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  const [backgroundImage, setBackgroundImage] = useState(null); // Updated to null initially
+  const [backgroundImage, setBackgroundImage] = useState(null);
+  const [emailError, setEmailError] = useState(false);
+  const inputRef = useRef(null);
+
   const functionOnPopUp = () => {
     setOpen(true);
   };
+
   const closePopUp = () => {
     setOpen(false);
+  };
+
+  const handleEmailChange = (e) => {
+    const emailValue = e.target.value;
+    setEmail(emailValue);
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    setEmailError(!emailRegex.test(emailValue));
   };
 
   const handleImageChange = (event) => {
@@ -198,15 +45,18 @@ const ContactInfoAdd = ({ handleAddContact }) => {
       setBackgroundImage(imageFile);
     }
   };
-  const inputRef = useRef(null);
 
   const handleImageClick = () => {
     inputRef.current.click();
-    console.log("input ref", inputRef.current);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (emailError) {
+      alert("Please enter a valid email address.");
+      return;
+    }
+
     try {
       const formData = new FormData();
       formData.append("title", title);
@@ -215,7 +65,7 @@ const ContactInfoAdd = ({ handleAddContact }) => {
       formData.append("phoneNum", phoneNum);
       formData.append("email", email);
       formData.append("address", address);
-      console.log(FormData)
+
       const response = await fetch(
         `${process.env.REACT_APP_API_BASE_URL}/contacts/addContacts`,
         {
@@ -225,20 +75,18 @@ const ContactInfoAdd = ({ handleAddContact }) => {
       );
 
       if (response.ok) {
-        // Check for successful status code
         setResponse("Contact registered");
-        // alert("Form submitted successfully!");
-        // handleAddContact(); // Call the function to handle contact addition
-        console.log(response.data); // Access response data if needed
+        alert("Form submitted successfully!");
+        handleAddContact();
       } else {
         throw new Error("Network response was not ok");
       }
     } catch (error) {
       console.error("Error:", error);
       setResponse("Error posting data.");
-      // alert("Error submitting form. Please try again.");
+      alert("Error submitting form. Please try again.");
     }
-    // setOpen(false);
+    setOpen(false);
   };
 
   return (
@@ -304,7 +152,14 @@ const ContactInfoAdd = ({ handleAddContact }) => {
               </Typography>
             </Grid>
             <Grid item xs={6}>
-              <input type="file" id="imageInput" ref={inputRef} accept="image/*" onChange={handleImageChange} />
+              <input
+                type="file"
+                id="imageInput"
+                ref={inputRef}
+                accept="image/*"
+                onChange={handleImageChange}
+                // style={{ display: 'none' }}
+              />
               <Button onClick={handleImageClick} variant="outlined">
                 Choose File
               </Button>
@@ -331,12 +186,15 @@ const ContactInfoAdd = ({ handleAddContact }) => {
             </Grid>
             <Grid item xs={6}>
               <TextField
-                label="Enter mail"
+                label="Enter email"
                 variant="outlined"
                 fullWidth
                 type="email"
+                required
                 value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={handleEmailChange}
+                error={emailError}
+                helperText={emailError ? 'Please enter a valid email address' : ''}
               />
             </Grid>
             <Grid item xs={6}>
